90ec8a8b2dc5f64e0cf83bff85ecf258913d24a1
minjae23 <010626kmj@gmail.com>
2025-10-10 10:41:36 +0900

    build: initialize Spring Boot project for territory service - pit 461

---
+++ src/main/java/com/pitterpetter/loventure/territory/PitterPetterTerritoryApplication.java
@@ -0,0 +1,13 @@
+package com.pitterpetter.loventure.territory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitterPetterTerritoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PitterPetterTerritoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pitterpetter/loventure/territory/PitterPetterTerritoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pitterpetter.loventure.territory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitterPetterTerritoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
